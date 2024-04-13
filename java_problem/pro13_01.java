6af70bb4479f6b0fb05b8997f26c310c7ca4f16c
jeong donggyun <163277674+jjdonggyun@users.noreply.github.com>
2024-04-14 03:23:15 +0900

    Create pro13_01.java

---
+++ java_problem/pro13_01.java
@@ -0,0 +1,17 @@
+/*
+	시작이 반이다!!
+	(백준)문제 번호 2557
+	문제 | Hello World!를 출력하시오
+	입력 | X
+	출력 | Hello World!를 출력하시오.
+*/
+package project04;
+
+public class pro13_01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
