8217ef47125fa8beb3ed9bc891d798a0a3e5c91e
jeong donggyun <163277674+jjdonggyun@users.noreply.github.com>
2024-04-15 01:48:14 +0900

    Add files via upload

---
+++ java_problem/pro15_03.java
@@ -0,0 +1,30 @@
+/*
+	(백준)문제 번호 10430
+	문제
+	(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+	(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+	세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+	입력 | 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+	출력 | 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+*/
+package project04;
+
+import java.util.Scanner;
+
+public class pro15_03 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a, b, c;
+		a = scan.nextInt();
+		b = scan.nextInt();
+		c = scan.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c) + (b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c) * (b%c))%c);
+
+	}
+
+}
