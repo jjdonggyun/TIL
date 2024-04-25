7c8e47a4dea80290387992340e75569ecebc1263
jeong donggyun <163277674+jjdonggyun@users.noreply.github.com>
2024-04-25 20:13:05 +0900

    Add files via upload

---
+++ java_problem/project_04/pro25_04.java
@@ -0,0 +1,33 @@
+/*
+	(백준)문제 번호 11654
+	문제
+	알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+	입력
+	알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+	출력
+	입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+*/
+package test_code;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class pro25_04 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		char c = (char)br.read();
+		int code = (int)c;
+		
+		bw.write(String.valueOf(code));
+		
+		br.close();
+		bw.close();
+	}
+
+}
