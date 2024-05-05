76fb5b018dc4c1800ddec3eb674d8605ce2cd880
jeong donggyun <163277674+jjdonggyun@users.noreply.github.com>
2024-05-05 17:32:35 +0900

    Add files via upload

---
+++ java_problem/project_05/pro05_01.java
@@ -0,0 +1,45 @@
+/*
+	(백준)문제 번호 10988
+	문제
+	알파벳 소문자로만 이루어진 단어가 주어진다. 이때, 이 단어가 팰린드롬인지 아닌지 확인하는 프로그램을 작성하시오.
+	팰린드롬이란 앞으로 읽을 때와 거꾸로 읽을 때 똑같은 단어를 말한다. 
+	level, noon은 팰린드롬이고, baekjoon, online, judge는 팰린드롬이 아니다.
+	입력
+	첫째 줄에 단어가 주어진다. 단어의 길이는 1보다 크거나 같고, 100보다 작거나 같으며, 알파벳 소문자로만 이루어져 있다.
+	출력
+	첫째 줄에 팰린드롬이면 1, 아니면 0을 출력한다.
+*/
+package project05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class pro05_01 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input = br.readLine();
+		boolean result = true;
+		
+		for(int i = 0; i < input.length()/2; i++) {
+			if(input.charAt(i)!=input.charAt(input.length()-1-i)) {
+				result = false;
+				break;
+			}
+		}
+		if(result) {
+			bw.write("1");
+		}else {
+			bw.write("0");
+		}
+		br.close();
+		bw.close();
+
+	}
+
+}
